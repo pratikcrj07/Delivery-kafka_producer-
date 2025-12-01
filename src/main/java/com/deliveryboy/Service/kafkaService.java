ed6a7db4ff26cabfa4e79550a359be0b00e3158e
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-01 22:07:09 +0545

    created listner too in different folder and combine with load two maven project

--- src/main/java/com/deliveryboy/Service/kafkaService.java
+++ src/main/java/com/deliveryboy/Service/kafkaService.java
@@ -1,6 +1,8 @@
 package com.deliveryboy.Service;
 
 import com.deliveryboy.config.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,13 @@ public class kafkaService {
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    private Logger logger= LoggerFactory.getLogger(kafkaService.class);
+
     public boolean updateLocation(String Location) {
 
         this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME, Location);
+        this.logger.info("message produced");
+
 return true;
 
     }
